86d511414ec260fcd949e7ccc3f5e2a48edcdef2
propanoid <propanoid@gmail.com>
2016-09-16 13:58:58 +0300

    tests infrastructure

---
+++ tests/main_test.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <ctime>
+
+#include "gtest/gtest.h"
+
+int main( int argc, char** argv )
+{
+    std::random_device rd;
+    std::mt19937 mt( rd() );
+
+    ::testing::InitGoogleTest( &argc, argv );
+
+    return RUN_ALL_TESTS();
+}
